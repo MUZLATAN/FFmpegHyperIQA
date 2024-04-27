c680d583b4d59d90393ad06d5fc733f873863e27
zlatan <blackshake@outlook.com>
2024-04-27 15:26:41 +0800

    bugfix

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ffmpeg_hyperIQA
- Evaluating Image Quality Based on HyperIQA
+# FFMPEG_HyperIQA
+ Evaluating Image Quality Based on HyperIQA https://openaccess.thecvf.com/content_CVPR_2020/papers/Su_Blindly_Assess_Image_Quality_in_the_Wild_Guided_by_a_CVPR_2020_paper.pdf
  # What is HyperIQA
  HyperIQA is a method for evaluating image quality. It is based on the idea that human vision system is more sensitive to high-frequency content than low-frequency content. Therefore, a metric that takes into account both low-frequency and high-frequency content is needed.for more infomation click here  [hyperIQA](https://github.com/SSL92/hyperIQA)
  # requirement
@@ -9,6 +9,25 @@ PyTorch
 TorchVision
 scipy
  ```
+# important
+you need install ffmpeg in your computer
+
+# Data And Model Path
+download model form here https://drive.usercontent.google.com/download?id=1OOUmnbvpGea0LIGpIWEbOyxfWx6UCiiE&export=download&authuser=0
+```python
+# pretrained model
+./pretrained/koniq_pretrained.pkl
+
+# input data
+./data
+
+```
+
+# Output data directory
+```
+output_video/
+```
+
 
 # Run sample
 ```
--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import os
+import time
 from pathlib import Path
 import Utils
 import glob
 import HIQA
 import shutil
 
-if __name__ == "__main__":
-    path = Path.cwd()
-    input_file = path.absolute().joinpath('zelda.mp4') # 输入视频文件路径
-    output_dir = "keyframes"  # 输出关键帧的目录
+def preprocess_video(input_file:str, key_frame_output_dir:str, threshold:float):
+    Utils.extract_frames(input_file, key_frame_output_dir)
+    if not os.path.exists(key_frame_output_dir):
+        os.mkdir(key_frame_output_dir)
 
-
-
-    Utils.extract_frames(input_file, output_dir)
-
+    # 读取关键帧
     hyper_IQA = HIQA.HIQA()
-    files = glob.glob(os.path.join(output_dir, '*.jpg'))
+    files = glob.glob(os.path.join(key_frame_output_dir, '*.jpg'))
     duration = len(files)
     timeline = [False for i in range(int(duration))]
     for file in files:
@@ -23,11 +21,11 @@ if __name__ == "__main__":
         resource = hyper_IQA.inference(file)
 
         file = file[0: len(file) - 4]
-        idx = str.find(file,'frame-')
+        idx = str.find(file, 'frame-')
         file = file[idx + 6:len(file)]
-        print(" ----- "+str(resource)+" ----- "+file)
-        if (resource > 35):
-            timeline[int(file)-1] = True
+        print(" ----- " + str(resource) + " ----- " + file)
+        if (resource > threshold):
+            timeline[int(file) - 1] = True
 
     if not os.path.exists("output_video"):
         os.mkdir("output_video")
@@ -41,8 +39,31 @@ if __name__ == "__main__":
         else:
             if is_continuous:
                 end_time = i
-                Utils.cut_video(input_file, "output_video/"+str(start_time)+"-"+str(end_time)+".mp4", str(start_time), str(end_time - start_time))
+                Utils.cut_video(input_file, "output_video/" +str(time.time())+"-"+ str(start_time) + "-" + str(end_time) + ".mp4",
+                                str(start_time), str(end_time - start_time))
             is_continuous = False
 
-    #shutil.rmtree("keframes")
+    shutil.rmtree("keyframes")
     #shutil.rmtree("output_video")
+
+def find_mp4_files(directory):
+    mp4_files = []
+    for file_path in glob.glob(os.path.join(directory, '*.mp4')):
+        mp4_files.append(file_path)
+    return mp4_files
+
+if __name__ == "__main__":
+    path = Path.cwd()
+    data_path = path.absolute().joinpath('data')  # 输入视频文件路径
+    output_dir = "keyframes"  # 输出关键帧的目录
+
+    if  os.path.exists(output_dir):
+        shutil.rmtree("keyframes")
+
+    for file in find_mp4_files(data_path):
+        print(file)
+        preprocess_video(file, output_dir, 70)
+
+
+
+
